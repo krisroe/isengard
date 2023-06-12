80d373e30ce27e83a99d5b9c99d03172c0ad0952
Chris Rowe <chris.rowe@myparadigm.com>
2023-06-12 14:21:02 -0500

    add missing location form

---
+++ TelnetClientWrapper/frmLocations.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace IsengardClient
+{
+    partial class frmLocations
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.treeLocations = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Enabled = false;
+            this.btnGo.Location = new System.Drawing.Point(204, 680);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnSet
+            // 
+            this.btnSet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSet.Enabled = false;
+            this.btnSet.Location = new System.Drawing.Point(123, 680);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(75, 23);
+            this.btnSet.TabIndex = 1;
+            this.btnSet.Text = "Set";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(285, 680);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // treeLocations
+            // 
+            this.treeLocations.Dock = System.Windows.Forms.DockStyle.Top;
+            this.treeLocations.HideSelection = false;
+            this.treeLocations.Location = new System.Drawing.Point(0, 0);
+            this.treeLocations.Name = "treeLocations";
+            this.treeLocations.Size = new System.Drawing.Size(372, 674);
+            this.treeLocations.TabIndex = 64;
+            this.treeLocations.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeLocations_AfterSelect);
+            // 
+            // frmLocations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 715);
+            this.Controls.Add(this.treeLocations);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSet);
+            this.Controls.Add(this.btnGo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLocations";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Locations";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TreeView treeLocations;
+    }
+}
---
+++ TelnetClientWrapper/frmLocations.cs
@@ -0,0 +1,121 @@
+﻿using QuickGraph;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace IsengardClient
+{
+    internal partial class frmLocations : Form
+    {
+        private AdjacencyGraph<Room, Exit> _map;
+        private IsengardMap _fullMap;
+        private bool _flying;
+        private bool _levitating;
+        private bool _isDay;
+        private int _level;
+
+        public frmLocations(IsengardMap fullMap, Room currentRoom, bool flying, bool levitating, bool isDay, int level)
+        {
+            InitializeComponent();
+
+            _fullMap = fullMap;
+            _map = fullMap.MapGraph;
+            CurrentRoom = currentRoom;
+            _flying = flying;
+            _levitating = levitating;
+            _isDay = isDay;
+            _level = level;
+
+            PopulateTree();
+        }
+
+        public Room CurrentRoom
+        {
+            get;
+            set;
+        }
+
+        public List<Exit> SelectedPath
+        {
+            get;
+            set;
+        }
+
+        private void PopulateTree()
+        {
+            foreach (Area a in _fullMap.Areas)
+            {
+                TreeNode tArea = new TreeNode(a.Name);
+                tArea.Tag = a;
+                treeLocations.Nodes.Add(tArea);
+                tArea.Expand();
+                a.Locations.Sort(new RoomComparer());
+                foreach (Room r in a.Locations)
+                {
+                    TreeNode tRoom = new TreeNode(r.ToString());
+                    tRoom.Tag = r;
+                    tArea.Nodes.Add(tRoom);
+                }
+            }
+        }
+
+        private class RoomComparer : IComparer<Room>
+        {
+            public int Compare(Room x, Room y)
+            {
+                int ret;
+                if (x.Experience1.HasValue != y.Experience1.HasValue)
+                {
+                    ret = x.Experience1.HasValue ? 1 : -1;
+                }
+                else if (!x.Experience1.HasValue && !y.Experience1.HasValue)
+                {
+                    ret = 0;
+                }
+                else
+                {
+                    ret = x.Experience1.Value.CompareTo(y.Experience1.Value);
+                }
+                return ret;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            CurrentRoom = (Room)treeLocations.SelectedNode.Tag;
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            Room selectedRoom = (Room)treeLocations.SelectedNode.Tag;
+            SelectedPath = MapComputation.ComputeLowestCostPath(this.CurrentRoom, selectedRoom, _map, _flying, _levitating, _isDay, _level);
+            if (SelectedPath == null)
+            {
+                MessageBox.Show("No path to target room found.", "Go to Room", MessageBoxButtons.OK);
+                return;
+            }
+            Close();
+        }
+
+        private void treeLocations_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            bool isCurrentRoom = false;
+            TreeNode tn = treeLocations.SelectedNode;
+            if (tn != null)
+            {
+                Room r = tn.Tag as Room;
+                if (r != null)
+                {
+                    isCurrentRoom = r == CurrentRoom;
+                }
+            }
+            btnSet.Enabled = !isCurrentRoom;
+            btnGo.Enabled = CurrentRoom != null && !isCurrentRoom;
+        }
+    }
+}
