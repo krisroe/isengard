32c5881ce554720db10dac5ce80ebb6004bebc48
Chris Rowe <chris.rowe@myparadigm.com>
2023-07-05 14:07:47 -0500

    disconnected form shows as center parent

--- TelnetClientWrapper/frmDisconnected.Designer.cs
+++ TelnetClientWrapper/frmDisconnected.Designer.cs
@@ -99,8 +99,9 @@
             this.Controls.Add(this.lblDisconnected);
             this.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmDisconnected";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Disconnected";
             this.ResumeLayout(false);
             this.PerformLayout();
