db4e58ec5219c2d883188f5f13e31658ecf03a72
Chris Rowe <chris.rowe@myparadigm.com>
2023-07-13 12:54:33 -0500

    add missing choose areas form

---
+++ TelnetClientWrapper/frmChooseAreas.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace IsengardClient
+{
+    partial class frmChooseAreas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlBottom = new System.Windows.Forms.Panel();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.chklst = new System.Windows.Forms.CheckedListBox();
+            this.pnlBottom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlBottom
+            // 
+            this.pnlBottom.Controls.Add(this.btnCancel);
+            this.pnlBottom.Controls.Add(this.btnOK);
+            this.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlBottom.Location = new System.Drawing.Point(0, 513);
+            this.pnlBottom.Name = "pnlBottom";
+            this.pnlBottom.Size = new System.Drawing.Size(474, 54);
+            this.pnlBottom.TabIndex = 0;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(156, 6);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(151, 37);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(313, 6);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(151, 37);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // chklst
+            // 
+            this.chklst.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chklst.FormattingEnabled = true;
+            this.chklst.Location = new System.Drawing.Point(0, 0);
+            this.chklst.Name = "chklst";
+            this.chklst.Size = new System.Drawing.Size(474, 513);
+            this.chklst.TabIndex = 1;
+            // 
+            // frmChooseAreas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 567);
+            this.Controls.Add(this.chklst);
+            this.Controls.Add(this.pnlBottom);
+            this.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimizeBox = false;
+            this.Name = "frmChooseAreas";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Choose Areas";
+            this.pnlBottom.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBottom;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.CheckedListBox chklst;
+    }
+}
---
+++ TelnetClientWrapper/frmChooseAreas.cs
@@ -0,0 +1,44 @@
+﻿using IsengardClient.Properties;
+using System.Collections.Generic;
+using System.Windows.Forms;
+namespace IsengardClient
+{
+    internal partial class frmChooseAreas : Form
+    {
+        public frmChooseAreas(IsengardSettingData settings, HashSet<Area> areas)
+        {
+            InitializeComponent();
+
+            List<Area> aList = new List<Area>();
+            foreach (Area a in settings.EnumerateAreas())
+            {
+                aList.Add(a);
+                chklst.Items.Add(a);
+            }
+            if (areas != null)
+            {
+                foreach (Area a in areas)
+                {
+                    chklst.SetItemChecked(aList.IndexOf(a), true);
+                }
+            }
+        }
+
+        public HashSet<Area> SelectedAreas
+        {
+            get
+            {
+                HashSet<Area> ret = null;
+                if (chklst.CheckedItems.Count > 0)
+                {
+                    ret = new HashSet<Area>();
+                    foreach (Area a in chklst.CheckedItems)
+                    {
+                        ret.Add(a);
+                    }
+                }
+                return ret;
+            }
+        }
+    }
+}
