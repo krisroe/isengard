dd4f4e19b56c770e60561a949f8680509bc6ef65
Chris Rowe <chris.rowe@myparadigm.com>
2024-06-04 15:16:44 -0500

    convert Graph# to new visual studio project file format

---
+++ Graph#/ShortestDistanceRelaxer.cs
@@ -0,0 +1,27 @@
+﻿using QuickGraph.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace GraphSharp
+{
+    public sealed class ShortestDistanceRelaxer : QuickGraph.Algorithms.IDistanceRelaxer
+    {
+        public static readonly ShortestDistanceRelaxer Instance;
+
+        double IDistanceRelaxer.InitialDistance => double.MaxValue;
+
+        double IDistanceRelaxer.Combine(double distance, double weight)
+        {
+            return distance + weight;
+        }
+
+        int IComparer<double>.Compare(double x, double y)
+        {
+            return x.CompareTo(y);
+        }
+    }
+}
